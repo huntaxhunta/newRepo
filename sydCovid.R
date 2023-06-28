874997302587d9c38218ab3f4769fdc0ab4b93dd
Jianwen Yang <yang_jw09@hotmail.com>
2023-06-28 20:01:02 +1000

    updated Rplot

--- sydCovid.R
+++ sydCovid.R
@@ -13,12 +13,13 @@ julyDec <- subset(covid, date > "2020-07-01" & date < "2020-12-31")
 
 g <- ggplot(julyDec) +
 	  geom_line(aes(date, H, col = "red")) +
-	  geom_line(aes(date, C)) +
+	  geom_line(aes(date, C, col = "Critical")) +
+	  geom_line(aes(date, D, col = "dead")) +
 	  scale_y_sqrt() +
 	  scale_x_date(breaks = "month",date_minor_breaks = "week",
 		      date_labels = "%B") +
-	  scale_y_log10()+
 	  labs(title = "hostpital bed between July and December",
 		        x = "date", y = "Hospital bed count")
+
 
 print(g)

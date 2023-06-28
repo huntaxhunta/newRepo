3bd0036eb9ea8eebf5effb1cb7f2a964aeda9da4
Jianwen Yang <yang_jw09@hotmail.com>
2023-06-28 14:04:07 +1000

    added covid analysis

---
+++ sydCovid.R
@@ -0,0 +1,19 @@
+library(ggplot2)
+
+# load data
+covid <- read.csv("epiSEIHCRD_combAge.csv") 
+
+# change the date format 
+covid$date <- as.Date(covid$t, origin = "2020-03-01")
+
+# Subset the data by time frame
+julyDec <- subset(covid, date > "2020-07-01" & date < "2020-12-31")
+
+#Plot the data
+
+g <- ggplot(julyDec) +
+	  geom_col(aes(date, H)) +
+	    labs(title = "hostpital bed between July and December",
+		        x = "date", y = "Hospital bed count")
+
+print(g)
